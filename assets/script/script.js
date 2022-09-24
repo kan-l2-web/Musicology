67f9629e4198fa16b53af4baaa2e7fc30a7bd7bc
kang-Demir <100368124+kan-l2-web@users.noreply.github.com>
2022-09-24 16:09:45 +0700

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,12 @@
-// Hello world
+// #####################################################################
+// #                                                                   #
+// #                         code by Kandemir                          #
+// #                                                                   #
+// #####################################################################
+
+let burger = document.querySelector('.hdr__burger');
+let nav = document.querySelector('.hdr__nav');
+
+burger.addEventListener('click', function(e){
+	nav.classList.toggle('active');
+});
